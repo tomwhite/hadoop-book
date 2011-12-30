6c8b8a728684e9e60c5f70cad04032765a37a40d
Tom White <tom@cloudera.com>
2011-12-29 17:33:34 -0800

    Add LoggingIdentityMapper.

---
+++ ch05/src/main/java/LoggingIdentityMapper.java
@@ -0,0 +1,28 @@
+//cc LoggingIdentityMapper An identity mapper that writes to standard output and also uses the Apache Commons Logging API
+import java.io.IOException;
+
+//vv LoggingIdentityMapper
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class LoggingIdentityMapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT>
+  extends Mapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT> {
+  
+  private static final Log LOG = LogFactory.getLog(LoggingIdentityMapper.class);
+  
+  @Override
+  public void map(KEYIN key, VALUEIN value, Context context)
+      throws IOException, InterruptedException {
+    // Log to stdout file
+    System.out.println("Map key: " + key);
+    
+    // Log to syslog file
+    LOG.info("Map key: " + key);
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("Map value: " + value);
+    }
+    context.write((KEYOUT) key, (VALUEOUT) value);
+  }
+}
+//^^ LoggingIdentityMapper
