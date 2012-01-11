e54d62750602c55213f50fe4d0b5096be6c37e92
Tom White <tom@cloudera.com>
2012-01-11 08:54:53 -0800

    Add some missing files.

---
+++ ch05/src/main/java/LoggingDriver.java
@@ -0,0 +1,36 @@
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class LoggingDriver extends Configured implements Tool {
+  
+  @Override
+  public int run(String[] args) throws Exception {
+    if (args.length != 2) {
+      System.err.printf("Usage: %s [generic options] <input> <output>\n",
+          getClass().getSimpleName());
+      ToolRunner.printGenericCommandUsage(System.err);
+      return -1;
+    }
+    
+    Job job = new Job(getConf(), "Logging job");
+    job.setJarByClass(getClass());
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
+    job.setMapperClass(LoggingIdentityMapper.class);
+    job.setNumReduceTasks(0);
+    
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
+  
+  public static void main(String[] args) throws Exception {
+    int exitCode = ToolRunner.run(new LoggingDriver(), args);
+    System.exit(exitCode);
+  }
+}
