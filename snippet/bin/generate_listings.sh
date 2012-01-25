834cab58469bdb91dc6c64682b364c22b4727ec7
Tom White <tom@cloudera.com>
2012-01-24 22:42:00 -0800

    Change a few Oozie paths.

--- snippet/bin/generate_listings.sh
+++ snippet/bin/generate_listings.sh
@@ -10,6 +10,7 @@ perl $bin/phragmite_db.pl $actual/ch02/ $(grep -ElR '(// ?cc|// ?==)' --include 
 perl $bin/phragmite_db.pl $actual/ch04/ $(grep -ElR '(// ?cc|// ?==)' --include '*.java' $bin/../../ch04)
 perl $bin/phragmite_db.pl $actual/ch04-avro/ $(grep -ElR '(// ?cc|// ?==)' --include '*.java' $bin/../../ch04-avro)
 perl $bin/phragmite_db.pl $actual/ch05/ $(grep -ElR '(// ?cc|// ?==)' --include '*.java' $bin/../../ch05)
+perl $bin/phragmite_db.pl $actual/ch05/ $bin/../../ch05/src/main/resources/oozie-examples/max-temp-workflow/workflow.xml
 perl $bin/phragmite_db.pl $actual/ch07/ $(grep -ElR '(// ?cc|// ?==)' --include '*.java' $bin/../../ch07)
 perl $bin/phragmite_db.pl $actual/common/ $(grep -ElR '(// ?cc|// ?==)' --include '*.java' $bin/../../common)
 perl $bin/phragmite_db.pl $actual/ch08/ $(grep -ElR '(// ?cc|// ?==)' --include '*.java' $bin/../../ch08)
