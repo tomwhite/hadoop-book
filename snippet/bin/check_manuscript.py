2b707b783a16f5efece66cc768e2322170365883
Tom White <tom@cloudera.com>
2012-02-01 09:47:32 -0800

    Fixed check_manuscript to ignore first comment line.

--- snippet/bin/check_manuscript.py
+++ snippet/bin/check_manuscript.py
@@ -9,7 +9,7 @@ manuscript = open(sys.argv[1], 'r').read()
 
 for snippet_file in sys.argv[2:]:
   lines = open(snippet_file, 'r').readlines()
-  if lines[0].startswith("<--"):
+  if lines[0].startswith("<!--"):
     doc = "".join(lines[1:]) # remove first line if a comment
   else:
     doc = "".join(lines[0:])
