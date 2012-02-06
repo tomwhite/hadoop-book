48924563ae4f5a3ebb44bc3a1a468c07d5507b86
Tom White <tom@cloudera.com>
2012-02-05 20:38:32 -0800

    Add better snippet highlighting.

--- ch02/src/main/java/OldMaxTemperature.java
+++ ch02/src/main/java/OldMaxTemperature.java
@@ -14,6 +14,7 @@ public class OldMaxTemperature {
   
     private static final int MISSING = 9999;
     
+    @Override
     public void map(LongWritable key, Text value,
         /*[*/OutputCollector<Text, IntWritable> output, Reporter reporter/*]*/)
         throws IOException {
@@ -36,13 +37,14 @@ public class OldMaxTemperature {
   static class OldMaxTemperatureReducer /*[*/extends MapReduceBase/*]*/
     /*[*/implements Reducer/*]*/<Text, IntWritable, Text, IntWritable> {
 
+    @Override
     public void reduce(Text key, /*[*/Iterator/*]*/<IntWritable> values,
         /*[*/OutputCollector<Text, IntWritable> output, Reporter reporter/*]*/)
         throws IOException {
       
       int maxValue = Integer.MIN_VALUE;
-      while (values.hasNext()) {
-        maxValue = Math.max(maxValue, values.next().get());
+      while (/*[*/values.hasNext()/*]*/) {
+        maxValue = Math.max(maxValue, /*[*/values.next().get()/*]*/);
       }
       /*[*/output.collect/*]*/(key, new IntWritable(maxValue));
     }
@@ -54,17 +56,17 @@ public class OldMaxTemperature {
       System.exit(-1);
     }
     
-    /*[*/JobConf conf = new JobConf(MaxTemperatureWithCombiner.class);/*]*/
-    conf.setJobName("Max temperature");
+    /*[*/JobConf conf = new JobConf(OldMaxTemperature.class);/*]*/
+    /*[*/conf/*]*/.setJobName("Max temperature");
 
-    FileInputFormat.addInputPath(conf, new Path(args[0]));
-    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+    FileInputFormat.addInputPath(/*[*/conf/*]*/, new Path(args[0]));
+    FileOutputFormat.setOutputPath(/*[*/conf/*]*/, new Path(args[1]));
     
-    conf.setMapperClass(OldMaxTemperatureMapper.class);
-    conf.setReducerClass(OldMaxTemperatureReducer.class);
+    /*[*/conf/*]*/.setMapperClass(OldMaxTemperatureMapper.class);
+    /*[*/conf/*]*/.setReducerClass(OldMaxTemperatureReducer.class);
 
-    conf.setOutputKeyClass(Text.class);
-    conf.setOutputValueClass(IntWritable.class);
+    /*[*/conf/*]*/.setOutputKeyClass(Text.class);
+    /*[*/conf/*]*/.setOutputValueClass(IntWritable.class);
 
     /*[*/JobClient.runJob(conf);/*]*/
   }
