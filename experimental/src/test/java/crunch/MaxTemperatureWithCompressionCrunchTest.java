53d9f4d07ff7147d38f23c6ead9a3c5cd7dd9481
Tom White <tom@cloudera.com>
2014-01-26 15:52:49 +0000

    Add example of using Hadoop configuration to set output compression

--- experimental/src/test/java/crunch/MaxTemperatureWithCompressionCrunchTest.java
+++ experimental/src/test/java/crunch/MaxTemperatureWithCompressionCrunchTest.java
@@ -1,12 +1,4 @@
 package crunch;
-import static org.apache.crunch.types.writable.Writables.ints;
-import static org.apache.crunch.types.writable.Writables.strings;
-import static org.apache.crunch.types.writable.Writables.tableOf;
-
-import java.io.IOException;
-
-import org.apache.crunch.fn.Aggregators;
-import org.junit.Test;
 
 import org.apache.crunch.DoFn;
 import org.apache.crunch.Emitter;
@@ -14,22 +6,37 @@ import org.apache.crunch.PCollection;
 import org.apache.crunch.PTable;
 import org.apache.crunch.Pair;
 import org.apache.crunch.Pipeline;
+import org.apache.crunch.fn.Aggregators;
 import org.apache.crunch.impl.mr.MRPipeline;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.junit.Test;
 
-public class MaxTemperatureCrunchTest {
+import java.io.IOException;
+
+import static org.apache.crunch.types.writable.Writables.ints;
+import static org.apache.crunch.types.writable.Writables.strings;
+import static org.apache.crunch.types.writable.Writables.tableOf;
+
+public class MaxTemperatureWithCompressionCrunchTest {
   
   private static final int MISSING = 9999;
   
   @Test
   public void test() throws IOException {
-    Pipeline pipeline = new MRPipeline(MaxTemperatureCrunchTest.class);
+    Configuration conf = new Configuration();
+    conf.setBoolean("mapreduce.output.fileoutputformat.compress", true);
+    conf.setClass("mapreduce.output.fileoutputformat.compress.codec", GzipCodec.class, CompressionCodec.class);
+    
+    Pipeline pipeline = new MRPipeline(MaxTemperatureWithCompressionCrunchTest.class, conf);
     PCollection<String> records = pipeline.readTextFile("input/ncdc/sample.txt");
     
     PTable<String, Integer> maxTemps = records
       .parallelDo(toYearTempPairsFn(), tableOf(strings(), ints()))
       .groupByKey()
       .combineValues(Aggregators.MAX_INTS());
-    
+
     pipeline.writeTextFile(maxTemps, "output");
     pipeline.run();
   }
