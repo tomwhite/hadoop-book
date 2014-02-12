af8f5bd3b9d53a67dca340bb07807fa71f8ed928
Tom White <tom@cloudera.com>
2014-02-12 10:04:32 +0000

    Moved MaxTemperatureWithCompressionCrunch

--- chxx-crunch/src/main/java/crunch/MaxTemperatureWithCompressionCrunch.java
+++ chxx-crunch/src/main/java/crunch/MaxTemperatureWithCompressionCrunch.java
@@ -1,12 +1,4 @@
 package crunch;
-import static org.apache.crunch.types.writable.Writables.ints;
-import static org.apache.crunch.types.writable.Writables.strings;
-import static org.apache.crunch.types.writable.Writables.tableOf;
-
-import java.io.IOException;
-
-import org.apache.crunch.fn.Aggregators;
-import org.junit.Test;
 
 import org.apache.crunch.DoFn;
 import org.apache.crunch.Emitter;
@@ -14,23 +6,40 @@ import org.apache.crunch.PCollection;
 import org.apache.crunch.PTable;
 import org.apache.crunch.Pair;
 import org.apache.crunch.Pipeline;
+import org.apache.crunch.fn.Aggregators;
 import org.apache.crunch.impl.mr.MRPipeline;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
 
-public class MaxTemperatureCrunchTest {
-  
+import static org.apache.crunch.types.writable.Writables.ints;
+import static org.apache.crunch.types.writable.Writables.strings;
+import static org.apache.crunch.types.writable.Writables.tableOf;
+
+// Crunch version of ch04 MaxTemperatureWithCompression
+public class MaxTemperatureWithCompressionCrunch {
+
   private static final int MISSING = 9999;
-  
-  @Test
-  public void test() throws IOException {
-    Pipeline pipeline = new MRPipeline(MaxTemperatureCrunchTest.class);
-    PCollection<String> records = pipeline.readTextFile("input/ncdc/sample.txt");
+
+  public static void main(String[] args) throws Exception {
+    if (args.length != 2) {
+      System.err.println("Usage: MaxTemperatureWithCompressionCrunch <input path> <output path>");
+      System.exit(-1);
+    }
+
+    Configuration conf = new Configuration();
+    conf.setBoolean("mapreduce.output.fileoutputformat.compress", true);
+    conf.setClass("mapreduce.output.fileoutputformat.compress.codec", GzipCodec.class, CompressionCodec.class);
+
+    Pipeline pipeline = new MRPipeline(MaxTemperatureWithCompressionCrunch.class, conf);
+    PCollection<String> records = pipeline.readTextFile(args[0]);
     
     PTable<String, Integer> maxTemps = records
       .parallelDo(toYearTempPairsFn(), tableOf(strings(), ints()))
       .groupByKey()
       .combineValues(Aggregators.MAX_INTS());
-    
-    pipeline.writeTextFile(maxTemps, "output");
+
+    pipeline.writeTextFile(maxTemps, args[1]);
     pipeline.run();
   }
 
