d5607d97fb97ddb660911024e6af0773e19ff2fc
Tom White <tom@cloudera.com>
2014-02-13 09:51:06 +0000

    Add SortByTemperatureCrunch

--- chxx-crunch/src/main/java/crunch/SortByTemperatureCrunch.java
+++ chxx-crunch/src/main/java/crunch/SortByTemperatureCrunch.java
@@ -1,6 +1,5 @@
 package crunch;
 
-import java.io.IOException;
 import org.apache.crunch.DoFn;
 import org.apache.crunch.Emitter;
 import org.apache.crunch.PCollection;
@@ -9,23 +8,26 @@ import org.apache.crunch.Pair;
 import org.apache.crunch.Pipeline;
 import org.apache.crunch.impl.mr.MRPipeline;
 import org.apache.crunch.lib.Sort;
-import org.junit.Test;
 
 import static org.apache.crunch.types.writable.Writables.ints;
 import static org.apache.crunch.types.writable.Writables.strings;
 import static org.apache.crunch.types.writable.Writables.tableOf;
 
-public class SortByTemperatureCrunchTest {
+// Crunch version of ch08 SortByTemperatureUsingTotalOrderPartitioner
+public class SortByTemperatureCrunch {
 
-  @Test
-  public void test() throws IOException {
-    Pipeline pipeline = new MRPipeline(MaxTemperatureAvroCrunchTest.class);
-    PCollection<String> records = pipeline.readTextFile("input/ncdc/sample.txt");
+  public static void main(String[] args) throws Exception {
+    if (args.length != 2) {
+      System.err.println("Usage: MaxTemperatureCrunch <input path> <output path>");
+      System.exit(-1);
+    }
+    Pipeline pipeline = new MRPipeline(SortByTemperatureCrunch.class);
+    PCollection<String> records = pipeline.readTextFile(args[0]);
     PTable<Integer, String> temps = records
         .parallelDo(toTempValuePairsFn(), tableOf(ints(), strings()));
 
     PTable<Integer, String> sorted = Sort.sort(temps, Sort.Order.DESCENDING);
-    pipeline.writeTextFile(sorted, "output");
+    pipeline.writeTextFile(sorted, args[1]);
     pipeline.run();
   }
 
