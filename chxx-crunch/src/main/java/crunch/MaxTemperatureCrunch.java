338fe09b032a2f1efcbb2ebc71513f4e434afd40
Tom White <tom@cloudera.com>
2014-03-02 11:53:07 +0000

    Make intermediate PTable clearer in first example.

--- chxx-crunch/src/main/java/crunch/MaxTemperatureCrunch.java
+++ chxx-crunch/src/main/java/crunch/MaxTemperatureCrunch.java
@@ -4,6 +4,7 @@ import static org.apache.crunch.types.writable.Writables.ints;
 import static org.apache.crunch.types.writable.Writables.strings;
 import static org.apache.crunch.types.writable.Writables.tableOf;
 
+import org.apache.crunch.PipelineResult;
 import org.apache.crunch.fn.Aggregators;
 
 import org.apache.crunch.DoFn;
@@ -17,8 +18,6 @@ import org.apache.crunch.impl.mr.MRPipeline;
 // Crunch version of ch02 MaxTemperature
 public class MaxTemperatureCrunch {
   
-  private static final int MISSING = 9999;
-  
   public static void main(String[] args) throws Exception {
     if (args.length != 2) {
       System.err.println("Usage: MaxTemperatureCrunch <input path> <output path>");
@@ -27,31 +26,26 @@ public class MaxTemperatureCrunch {
 
     Pipeline pipeline = new MRPipeline(MaxTemperatureCrunch.class);
     PCollection<String> records = pipeline.readTextFile(args[0]);
-    
-    PTable<String, Integer> maxTemps = records
-      .parallelDo(toYearTempPairsFn(), tableOf(strings(), ints()))
+
+    PTable<String, Integer> yearTemperatures = records
+        .parallelDo(toYearTempPairsFn(), tableOf(strings(), ints()));
+    PTable<String, Integer> maxTemps = yearTemperatures
       .groupByKey()
       .combineValues(Aggregators.MAX_INTS());
     
     pipeline.writeTextFile(maxTemps, args[1]);
-    pipeline.run();
+    PipelineResult result = pipeline.run();
+    System.exit(result.succeeded() ? 0 : 1);
   }
 
-  private static DoFn<String, Pair<String, Integer>> toYearTempPairsFn() {
+  static DoFn<String, Pair<String, Integer>> toYearTempPairsFn() {
     return new DoFn<String, Pair<String, Integer>>() {
+      NcdcRecordParser parser = new NcdcRecordParser();
       @Override
       public void process(String input, Emitter<Pair<String, Integer>> emitter) {
-        String line = input.toString();
-        String year = line.substring(15, 19);
-        int airTemperature;
-        if (line.charAt(87) == '+') { // parseInt doesn't like leading plus signs
-          airTemperature = Integer.parseInt(line.substring(88, 92));
-        } else {
-          airTemperature = Integer.parseInt(line.substring(87, 92));
-        }
-        String quality = line.substring(92, 93);
-        if (airTemperature != MISSING && quality.matches("[01459]")) {
-          emitter.emit(Pair.of(year, airTemperature));
+        parser.parse(input);
+        if (parser.isValidTemperature()) {
+          emitter.emit(Pair.of(parser.getYear(), parser.getAirTemperature()));
         }
       }
     };
