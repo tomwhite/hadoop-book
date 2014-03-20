ed1963ad4233afb5f024f209bf38db1f72bdda0a
Tom White <tom@cloudera.com>
2014-03-20 11:54:27 +0000

    Move dump() methods into static helper class.

---
+++ chxx-crunch/src/test/java/crunch/PCollections.java
@@ -0,0 +1,56 @@
+package crunch;
+
+import java.util.Iterator;
+import org.apache.crunch.MapFn;
+import org.apache.crunch.PCollection;
+import org.apache.crunch.PGroupedTable;
+import org.apache.crunch.PTable;
+import org.apache.crunch.Pair;
+
+import static org.apache.crunch.types.writable.Writables.strings;
+
+public class PCollections {
+
+  public static <S> String dump(PCollection<S> collection) {
+    StringBuilder sb = new StringBuilder("{");
+    for (Iterator<S> i = collection.materialize().iterator(); i.hasNext(); ) {
+      sb.append(i.next());
+      if (i.hasNext()) {
+        sb.append(",");
+      }
+    }
+    sb.append("}");
+    return sb.toString();
+  }
+
+  public static <K, V> String dump(PTable<K, V> table) {
+    StringBuilder sb = new StringBuilder("{");
+    for (Iterator<Pair<K, V>> i = table.materialize().iterator(); i.hasNext(); ) {
+      Pair<K, V> pair = i.next();
+      sb.append("(").append(pair.first()).append(",").append(pair.second()).append(")");
+      if (i.hasNext()) {
+        sb.append(",");
+      }
+    }
+    sb.append("}");
+    return sb.toString();
+  }
+
+  public static <K, V> String dump(PGroupedTable<K, V> groupedTable) {
+    return dump(groupedTable.mapValues(new MapFn<Iterable<V>, String>() {
+      @Override
+      public String map(Iterable<V> input) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (Iterator<V> i = input.iterator(); i.hasNext(); ) {
+          sb.append(i.next());
+          if (i.hasNext()) {
+            sb.append(",");
+          }
+        }
+        sb.append("]");
+        return sb.toString();
+      }
+    }, strings()));
+  }
+}
--- chxx-crunch/src/test/java/crunch/PrimitiveOperationsTest.java
+++ chxx-crunch/src/test/java/crunch/PrimitiveOperationsTest.java
@@ -21,6 +21,7 @@ import org.apache.crunch.test.TemporaryPath;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static crunch.PCollections.dump;
 import static org.apache.crunch.types.writable.Writables.doubles;
 import static org.apache.crunch.types.writable.Writables.ints;
 import static org.apache.crunch.types.writable.Writables.strings;
@@ -213,48 +214,5 @@ public class PrimitiveOperationsTest implements Serializable {
       }
     }, strings());
     assertEquals("{(6,c),(5,a),(6,b)}", dump(f));
-  }
-
-  private <S> String dump(PCollection<S> collection) {
-    StringBuilder sb = new StringBuilder("{");
-    for (Iterator<S> i = collection.materialize().iterator(); i.hasNext(); ) {
-      sb.append(i.next());
-      if (i.hasNext()) {
-        sb.append(",");
-      }
-    }
-    sb.append("}");
-    return sb.toString();
-  }
-
-  private <K, V> String dump(PTable<K, V> table) {
-    StringBuilder sb = new StringBuilder("{");
-    for (Iterator<Pair<K, V>> i = table.materialize().iterator(); i.hasNext(); ) {
-      Pair<K, V> pair = i.next();
-      sb.append("(").append(pair.first()).append(",").append(pair.second()).append(")");
-      if (i.hasNext()) {
-        sb.append(",");
-      }
-    }
-    sb.append("}");
-    return sb.toString();
-  }
-
-  private <K, V> String dump(PGroupedTable<K, V> groupedTable) {
-    return dump(groupedTable.mapValues(new MapFn<Iterable<V>, String>() {
-      @Override
-      public String map(Iterable<V> input) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        for (Iterator<V> i = input.iterator(); i.hasNext(); ) {
-          sb.append(i.next());
-          if (i.hasNext()) {
-            sb.append(",");
-          }
-        }
-        sb.append("]");
-        return sb.toString();
-      }
-    }, strings()));
   }
 }
