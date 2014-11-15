d972a7ac964f51e777d185d3a6585762f4b03b90
Tom White <tom@cloudera.com>
2014-11-15 18:04:28 +0000

    Use TableName class rather than bytes.

--- ch20-hbase/src/main/java/ExampleClient.java
+++ ch20-hbase/src/main/java/ExampleClient.java
@@ -3,6 +3,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
@@ -13,23 +14,23 @@ import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class ExampleClient {
+
   public static void main(String[] args) throws IOException {
     Configuration config = HBaseConfiguration.create();
-
     // Create table
     HBaseAdmin admin = new HBaseAdmin(config);
-    HTableDescriptor htd = new HTableDescriptor("test");
+    TableName tableName = TableName.valueOf("test");
+    HTableDescriptor htd = new HTableDescriptor(tableName);
     HColumnDescriptor hcd = new HColumnDescriptor("data");
     htd.addFamily(hcd);
     admin.createTable(htd);
-    byte[] tablename = htd.getName();
     HTableDescriptor[] tables = admin.listTables();
-    if (tables.length != 1 && Bytes.equals(tablename, tables[0].getName())) {
+    if (tables.length != 1 &&
+        Bytes.equals(tableName.getName(), tables[0].getTableName().getName())) {
       throw new IOException("Failed create of table");
     }
-
     // Run some operations -- three puts, a get, and a scan -- against the table.
-    HTable table = new HTable(config, tablename);
+    HTable table = new HTable(config, tableName);
     for (int i = 1; i <= 3; i++) {
       byte[] row = Bytes.toBytes("row" + i);
       Put put = new Put(row);
@@ -39,11 +40,9 @@ public class ExampleClient {
       put.add(columnFamily, qualifier, value);
       table.put(put);
     }
-
     Get get = new Get(Bytes.toBytes("row1"));
     Result result = table.get(get);
     System.out.println("Get: " + result);
-
     Scan scan = new Scan();
     ResultScanner scanner = table.getScanner(scan);
     try {
@@ -53,9 +52,8 @@ public class ExampleClient {
     } finally {
       scanner.close();
     }
-
     // Disable then drop the table
-    admin.disableTable(tablename);
-    admin.deleteTable(tablename);
+    admin.disableTable(tableName);
+    admin.deleteTable(tableName);
   }
 }
