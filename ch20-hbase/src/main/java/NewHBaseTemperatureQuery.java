cc9c184446129421eb07bdd76a2a95e5cff6a1a6
Tom White <tom@cloudera.com>
2014-11-16 15:52:07 +0000

    Add version of HBase code that uses the HBase 1.0 API for Connection, Admin, and Table.

---
+++ ch20-hbase/src/main/java/NewHBaseTemperatureQuery.java
@@ -0,0 +1,84 @@
+import java.io.IOException;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+/**
+ * HBase 1.0 version of HBaseTemperatureQuery that uses {@code Connection},
+ * and {@code Table}.
+ */
+public class NewHBaseTemperatureQuery extends Configured implements Tool {
+  static final byte[] DATA_COLUMNFAMILY = Bytes.toBytes("data");
+  static final byte[] AIRTEMP_QUALIFIER = Bytes.toBytes("airtemp");
+  
+  public NavigableMap<Long, Integer> getStationObservations(Table table,
+      String stationId, long maxStamp, int maxCount) throws IOException {
+    byte[] startRow = RowKeyConverter.makeObservationRowKey(stationId, maxStamp);
+    NavigableMap<Long, Integer> resultMap = new TreeMap<Long, Integer>();
+    Scan scan = new Scan(startRow);
+    scan.addColumn(DATA_COLUMNFAMILY, AIRTEMP_QUALIFIER);
+    ResultScanner scanner = table.getScanner(scan);
+    try {
+      Result res;
+      int count = 0;
+      while ((res = scanner.next()) != null && count++ < maxCount) {
+        byte[] row = res.getRow();
+        byte[] value = res.getValue(DATA_COLUMNFAMILY, AIRTEMP_QUALIFIER);
+        Long stamp = Long.MAX_VALUE -
+          Bytes.toLong(row, row.length - Bytes.SIZEOF_LONG, Bytes.SIZEOF_LONG);
+        Integer temp = Bytes.toInt(value);
+        resultMap.put(stamp, temp);
+      }
+    } finally {
+      scanner.close();
+    }
+    return resultMap;
+  }
+
+  public int run(String[] args) throws IOException {
+    if (args.length != 1) {
+      System.err.println("Usage: HBaseTemperatureQuery <station_id>");
+      return -1;
+    }
+
+    Configuration config = HBaseConfiguration.create();
+    Connection connection = ConnectionFactory.createConnection(config);
+    try {
+      TableName tableName = TableName.valueOf("observations");
+      Table table = connection.getTable(tableName);
+      try {
+        NavigableMap<Long, Integer> observations =
+            getStationObservations(table, args[0], Long.MAX_VALUE, 10).descendingMap();
+        for (Map.Entry<Long, Integer> observation : observations.entrySet()) {
+          // Print the date, time, and temperature
+          System.out.printf("%1$tF %1$tR\t%2$s\n", observation.getKey(),
+              observation.getValue());
+        }
+        return 0;
+      } finally {
+        table.close();
+      }
+    } finally {
+      connection.close();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    int exitCode = ToolRunner.run(HBaseConfiguration.create(),
+        new NewHBaseTemperatureQuery(), args);
+    System.exit(exitCode);
+  }
+}
